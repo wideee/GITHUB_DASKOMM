219f3e17bff76ec85bf706335aecfec13546338a
wideee <wmadearya@gmail.com>
2021-05-22 20:57:29 +0700

    dua

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home0004');
+});
+
+Route::get('/artikel', function () {
+    return view('artikel0004');
+});
+
+Route::get('/contact', function () {
+    return view('contact0004');
 });
